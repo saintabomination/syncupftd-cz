62e08924e9240cdd2ba485e61f637b6280f6aa2d
saintabomination <saint.abomination@protonmail.com>
2022-08-09 16:42:43 +0200

    Getting the current date

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+from datetime import date
+from datetime import timedelta
+
+today = date.today()
+yesterday = today - timedelta(days = 1)
+tomorrow = today + timedelta(days = 1)
+
+print(f'Yesterday: {yesterday}')
+print(f'Today: {today}')
+print(f'Tomorrow: {tomorrow}')
