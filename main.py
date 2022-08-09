0287824da49ba7bab48f08266ae67b28978e99e2
saintabomination <saint.abomination@protonmail.com>
2022-08-09 17:33:31 +0200

    Printing the current time and date

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 from datetime import date
+from datetime import datetime
 from datetime import timedelta
 
+DATE_FORMAT = '%d.%m.%Y'
+TIME_FORMAT = '%H:%M:%S'
 NAME_DAY_API = 'https://svatkyapi.cz/api'
 
 def get_name_day(date):
@@ -12,10 +15,14 @@ def get_name_day(date):
     except:
         return 'N/A'
     
+current_time = datetime.now()
 today = date.today()
 yesterday = today - timedelta(days = 1)
 tomorrow = today + timedelta(days = 1)
 
+print('SyncUpFTD spuštěn.')
+print(current_time.strftime(f'{DATE_FORMAT} {TIME_FORMAT}'))
+print('\nSvátky\n')
 print(f'Yesterday: {get_name_day(yesterday)}')
 print(f'Today: {get_name_day(today)}')
 print(f'Tomorrow: {get_name_day(tomorrow)}')
