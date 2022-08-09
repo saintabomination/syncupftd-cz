7f4990ccb95707e82334ae636bd19d040170b961
saintabomination <saint.abomination@protonmail.com>
2022-08-09 17:10:06 +0200

    Fetching name days via an API

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import requests
 from datetime import date
 from datetime import timedelta
+
+NAME_DAY_API = 'https://svatkyapi.cz/api'
+
+def get_name_day(date):
+    request = requests.get(f'{NAME_DAY_API}/day/{date}')
+    
+    if request.status_code != 200:
+        return 'N/A'
+
+    data = request.json()
+    return data['name']
 
 today = date.today()
 yesterday = today - timedelta(days = 1)
 tomorrow = today + timedelta(days = 1)
 
-print(f'Yesterday: {yesterday}')
-print(f'Today: {today}')
-print(f'Tomorrow: {tomorrow}')
+print(f'Yesterday: {get_name_day(yesterday)}')
+print(f'Today: {get_name_day(today)}')
+print(f'Tomorrow: {get_name_day(tomorrow)}')
