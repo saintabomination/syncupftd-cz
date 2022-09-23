5789e2e0f0770905b187eba1de5cc32a1af6e0c7
saintabomination <saint.abomination@protonmail.com>
2022-09-23 21:38:56 +0200

    Making the default currency more customizable

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from datetime import date
 from datetime import datetime
 from datetime import timedelta
 
-EXCHANGE_RATE_API="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest"
+EXCHANGE_RATE_API='https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest'
+TARGET_CURRENCY='czk'
 DATE_FORMAT = '%d.%m.%Y'
 TIME_FORMAT = '%H:%M:%S'
 NAME_DAY_API = 'https://svatkyapi.cz/api'
 
-def get_exchange_rate(input_currency):
+def get_exchange_rate(input_currency, output_currency):
     try:
-        request = requests.get(f'{EXCHANGE_RATE_API}/currencies/{input_currency}/czk.json')
+        request = requests.get(f'{EXCHANGE_RATE_API}/currencies/{input_currency}/{output_currency}.json')
         data = request.json()
-        return data['czk']
+        return data[TARGET_CURRENCY]
     except:
         return 'N/A'
 
@@ -33,9 +34,9 @@ print('SyncUpFTD spuštěn.')
 print(current_time.strftime(f'{DATE_FORMAT} {TIME_FORMAT}'))
 
 print('\nMěny\n')
-print(f'USD: {get_exchange_rate("usd")} CZK')
-print(f'EUR: {get_exchange_rate("eur")} CZK')
-print(f'GPB: {get_exchange_rate("gbp")} CZK')
+print(f'USD: {get_exchange_rate("usd", TARGET_CURRENCY)} {TARGET_CURRENCY.upper()}')
+print(f'EUR: {get_exchange_rate("eur", TARGET_CURRENCY)} {TARGET_CURRENCY.upper()}')
+print(f'GPB: {get_exchange_rate("gbp", TARGET_CURRENCY)} {TARGET_CURRENCY.upper()}')
 
 print('\nSvátky\n')
 print(f'Yesterday: {get_name_day(yesterday)}')
